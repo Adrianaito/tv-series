f825404fdae729c73fc806d850edaea2b220c3c0
Adriana Ito <ito.dri@gmail.com>
2021-02-22 00:15:14 +0900

    adding validation for photo

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,6 +1,6 @@
 class Serie < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :title, presence: true
-  # validates :rating, presence: true
   has_one_attached :photo
+  validates :photo, presence: true
 end
